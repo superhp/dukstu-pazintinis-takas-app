6c778de9e76d124e93f3985bbea5fc55333a612b
Gerda Simoliunaite <gerda.simoliunaite@storebrand.no>
2018-01-30 10:12:33 +0200

    Drawer implementation

--- ParkApp/About.js
+++ ParkApp/About.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, ControlPanel } from 'react-native';
 import { Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import DrawerMenu from './DrawerMenu';
 
 
 export default class About extends React.Component {
@@ -14,8 +15,17 @@ export default class About extends React.Component {
      
       <View style={styles.container}>
         <Text>This is infoooo</Text>
+        <Button onPress={this.openControlPanel}
+                title="Drawer"
+                color="#841584"
+                accessibilityLabel="Learn more about this blue button"
+                style={styles.actionButton}>
+        </Button>
+        {/*<DrawerMenu
+        
+          >
+        </DrawerMenu>*/}
       </View>
-      
     );
   }
 }
@@ -24,5 +34,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f3f3f3'
+  },
+  actionButton: {
+    position: "relative",
+    width: Dimensions.get("window").width,
+    height: 20,
+    zIndex: 1
   }
 });
