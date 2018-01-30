ccf123a621cf7fc5380b127c219b151b6acbbc99
Gerda Simoliunaite <gerda.simoliunaite@storebrand.no>
2018-01-30 16:22:10 +0200

    reversed commit

--- ParkApp/About.js
+++ ParkApp/About.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, ControlPanel } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import DrawerMenu from './DrawerMenu';
 
 
 export default class About extends React.Component {
@@ -15,18 +14,8 @@ export default class About extends React.Component {
      
       <View style={styles.container}>
         <Text>This is infoooo</Text>
-        <Text>Just trying from new computer</Text>
-        <Button onPress={this.openControlPanel}
-                title="Drawer"
-                color="#841584"
-                accessibilityLabel="Learn more about this blue button"
-                style={styles.actionButton}>
-        </Button>
-        {/*<DrawerMenu
-        
-          >
-        </DrawerMenu>*/}
       </View>
+      
     );
   }
 }
@@ -35,11 +24,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f3f3f3'
-  },
-  actionButton: {
-    position: "relative",
-    width: Dimensions.get("window").width,
-    height: 20,
-    zIndex: 1
   }
 });
